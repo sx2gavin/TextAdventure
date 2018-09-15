2497fb5177a6fbe45bd21937c5158bb655e4b5db
Gavin Luo <luoxiangyu528@gmail.com>
2018-09-14 23:27:59 -0400

    Initial commit for Gavin's text adventure game.

---
+++ Assets/MainGame.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainGame : MonoBehaviour {
+
+    [SerializeField] Text text;
+    [SerializeField] State CurrentState;
+
+	// Use this for initialization
+	void Start ()
+    {
+        UpdateText();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            IList<State> states = CurrentState.GetAllNextStates();
+            CurrentState = states[0];
+            UpdateText();
+        }
+	}
+
+    private void UpdateText()
+    {
+        text.text = CurrentState.TextContent;
+    }
+}
---
+++ Assets/State.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State", order = 1)]
+public class State : ScriptableObject {
+
+    [TextArea(15, 20)]
+    public string TextContent;
+
+    [SerializeField]
+    public List<State> NextStates;
+
+    public IList<State> GetAllNextStates()
+    {
+        return NextStates;
+    } 
+}
